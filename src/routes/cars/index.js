8a32f968d16c8217d9fbda356cb715d08e480345
Alan Torres <alan9805@gmail.com>
2021-07-10 15:04:51 -0500

    Routes

--- src/routes/cars/index.js
+++ src/routes/cars/index.js
@@ -1,4 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/bestOptionsPerYear/:year', (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.error(`[Error: ${error.message}]`);
+        if (error && error.statusCode) {
+            res.status(error.statusCode).send({ message: error.message });
+        } else {
+            res.status(500).send({ message: error.message });
+        }
+    }
+});
+
+router.post('/quoteCar', (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.error(`[Error: ${error.message}]`);
+        if (error && error.statusCode) {
+            res.status(error.statusCode).send({ message: error.message });
+        } else {
+            res.status(500).send({ message: error.message });
+        }
+    }
+});
+
 module.exports = router;
